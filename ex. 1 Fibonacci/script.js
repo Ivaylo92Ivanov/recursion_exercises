a348718b2fba719bd7acbc16c1154b78c67dae1b
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-11-26 16:03:09 +0200

    Refactor

--- ex. 1 Fibonacci/script.js
+++ ex. 1 Fibonacci/script.js
@@ -5,26 +5,27 @@
 // Now write another function recursionFibonacci which solves the same problem recursively.
 
 function iterationFibonacci(num) {
-  let fib1 = 0;
-  let fib2 = 1;
-  let result = [fib1, fib2];
   if (num<=0) return [];
-  if (num===1) return [fib1];
-  if (num===2) return [fib1, fib2];
+  if (num===1) return [0];
+  if (num===2) return [0, 1];
+
+  let result = [0, 1];
   for (let i=1; i<num-1; i++) {
     result.push(result[i] + result[i-1])
-  }
-  return result
+  };
+
+  return result;
 }   
 
 function recursionFibonacci(num) {
-  if (num===0) return [];
+  if (num<=0) return [];
   if (num===1) return [0];
   if (num===2) return [0, 1]; 
-  let currentResult = recursionFibonacci(num-1);
-  let currentIdx = num-2;
+
   let prevIdx = num-3;
-  let nextNumberToAppend = currentResult[currentIdx] + currentResult[prevIdx];
-  currentResult.push(nextNumberToAppend);
-  return currentResult;
+  let currentIdx = num-2;
+  let result = recursionFibonacci(num-1);
+  let nextFibNumber = result[currentIdx] + result[prevIdx];
+  result.push(nextFibNumber);
+  return result;
 }
